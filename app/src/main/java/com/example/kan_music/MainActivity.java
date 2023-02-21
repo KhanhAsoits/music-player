15074d0d237d40e0fffef0715c2d6d4a96d24fd9
035201000354 <hieu.dbutcl@gmail.com>
2023-02-21 15:11:22 +0700

    setup project

---
+++ app/src/main/java/com/example/kan_music/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.kan_music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, MusicListFragment.class, null)
+                    .commit();
+        }
+
+    }
+}
